b5c32f917bbbb683fceeeb94dcd548ab24335e08
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-07-16 15:27:32 -0700

    Created functions for badging and adding license section and link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,25 +2,57 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-    const reqLicense = license
+    const reqLicense = license;
     console.log(reqLicense);
-
-  }
-  return null
-}
+    const badgeURL = "https://img.shields.io/badge/License-";
+    switch (reqLicense) {
+      case 'MIT':
+        const mitURL = "MIT-yellow.svg";
+        finishedURL = (badgeURL+mitURL);
+        break;
+      case 'APACHE 2.0':
+        const apacheURL = "Apache_2.0-blue.svg";
+        finishedURL = (badgeURL+apacheURL);
+        break;
+      case 'GPL 3.0':
+        const gplURL = "GPLv3-blue.svg";
+        finishedURL = (badgeURL+gplURL);
+        break;
+      case 'BDS 3':
+        const bdsURL = "BSD_3--Clause-blue.svg";
+        finishedURL = (badgeURL+bdsURL);
+        break;
+      };
+      console.log(finishedURL);
+      const badge = (`![GitHub license](${finishedURL})`);
+      console.log(badge);
+    return badge;
+  };
+  return "";
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `\n* [License](#License)\n`
+  }
+  return '';
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return`## License
+    This project is licensed under ${license} license.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  renderLicenseBadge(data.license)
   return `# ${data.projName}
+  ${renderLicenseBadge(data.license)}
 
   ## Description
 
@@ -32,6 +64,8 @@ function generateMarkdown(data) {
 
   * [Usage](#Usage)
 
+  ${renderLicenseLink(data.license)}
+  
   * [Contributing](#Contributing)
 
   * [Tests](#Tests)
@@ -54,7 +88,7 @@ function generateMarkdown(data) {
 
   ## Contributing
 
-  ${data.contributing}
+  ${data.contribution}
 
   ## Tests
 
